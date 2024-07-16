0ef811bccd8c88a38579ffe98be6d633910e36d5
Your Name <you@example.com>
2024-07-16 08:54:34 +0530

    Add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,32 @@
-// Footer.js
-
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+    const buttons = [
+        { name: 'Leaderboard', routeUrl: '/leaderboard' },
+        { name: 'Mine', routeUrl: '/mine' },
+        { name: 'Tasks', routeUrl: '/tasks' },
+        { name: 'Profile', routeUrl: '/profile' },
+        { name: 'Quiz', routeUrl: '/quiz' }
+    ];
+
     return (
-        <footer className="bg-gray-800 text-gray-300 fixed bottom-0 w-full">
-            <div className="container mx-auto flex items-center justify-center">
-                <p className="text-sm">&copy; 2024 Accrue Quest. All rights reserved.</p>
-                <div className="flex space-x-6"> {/* Increased space between items */}
-                    <a href="#" className="text-sm hover:text-blue-500 px-2 py-1">Terms & Privacy</a>
+        <footer className="bg-blue-200 fixed bottom-0 w-full">
+            <div className="container mx-auto flex items-center justify-center py-2">
+                <div className="flex justify-center w-full">
+                    {buttons.map((button, index) => (
+                        <Link
+                            key={index}
+                            to={button.routeUrl}
+                            className="flex-1 bg-blue-600 text-white text-sm hover:bg-blue-500 px-4 py-2 rounded text-center"
+                        >
+                            {button.name}
+                        </Link>
+                    ))}
                 </div>
-                <span className="text-sm"></span>
-                <a href="#" className="text-sm hover:text-blue-500 px-2 py-1">Contact</a>
             </div>
         </footer>
     );
-}
+};
 
 export default Footer;
