8d1f74a63eb793aa388dbb7f31d6e898eebb44f3
Your Name <you@example.com>
2024-07-17 09:05:04 +0530

    add proper footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Footer = () => {
     const buttons = [
-        { name: 'Quiz', routeUrl: '/quiz' },
-        { name: 'Mine', routeUrl: '/mine' },
-        { name: 'Tasks', routeUrl: '/tasks' },
-        { name: 'Profile', routeUrl: '/profile' },
-        { name: 'Leaderboard', routeUrl: '/leaderboard' }
+        { name: 'Quiz', routeUrl: '/quiz', icon: 'fa-solid fa-exchange-alt' },
+        { name: 'Mine', routeUrl: '/mine', icon: 'fa-solid fa-gem' },
+        { name: 'Profile', routeUrl: '/profile', icon: 'fa-solid fa-user-friends' },
+        { name: 'Leaderboard', routeUrl: '/leaderboard', icon: 'fa-solid fa-coins' },
+        { name: 'Tasks', routeUrl: '/tasks', icon: 'fa-solid fa-parachute-box' },
+        { name: 'Wallet', routeUrl: '/wallet', icon: 'fa-solid fa-parachute-box' }
     ];
 
     return (
-        <footer className="bg-blue-200 fixed bottom-0 left-0 w-full">
-            <div className="container mx-auto flex flex-col items-center justify-between py-1 md:py-2 md:flex-row md:justify-between">
-                {/* Buttons */}
-                <div className="flex flex-wrap justify-center gap-2 w-full">
-                    {buttons.map((button, index) => (
-                        <Link
-                            key={index}
-                            to={button.routeUrl}
-                            className={`bg-blue-600 text-white text-xs hover:bg-blue-500 px-3 py-1 rounded text-center flex-1 min-w-[100px] 
-                            ${index === 0 ? 'md:ml-2' : ''} 
-                            ${index === buttons.length - 1 ? 'md:mr-2' : ''}`}
-                        >
-                            {button.name}
-                        </Link>
-                    ))}
-                </div>
+        <footer className="fixed bottom-2 left-0 w-full flex justify-center">
+            <div className="bg-blue-200 flex justify-around items-center w-full max-w-xs rounded-lg shadow-lg py-1 px-2">
+                {buttons.map((button, index) => (
+                    <Link
+                        key={index}
+                        to={button.routeUrl}
+                        className="flex flex-col items-center text-blue-800 text-xs hover:text-blue-600 m-1"
+                    >
+                        <i className={`${button.icon} text-xl mb-1`}></i>
+                        <span className="text-[10px]">{button.name}</span>
+                    </Link>
+                ))}
             </div>
         </footer>
     );
