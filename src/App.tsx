886122a34ad0f7c6c893716cecae035570ae27bd
Your Name <you@example.com>
2024-07-19 19:02:29 +0530

    git push

--- src/App.tsx
+++ src/App.tsx
@@ -127,12 +127,4 @@ const App = () => {
     );
 };
 
-const NotFound = () => (
-    <div>
-        <h1>404 - Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-        <Navigate to="/" />
-    </div>
-);
-
 export default App;
